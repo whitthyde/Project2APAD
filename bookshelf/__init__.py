b0401b3e0e449d9b6056360659eb171cc6c64d99
Whitt Hyde <whyde@utexas.edu>
2019-08-09 13:35:09 -0500

    Commiting new configuration settings

--- bookshelf/__init__.py
+++ bookshelf/__init__.py
@@ -19,40 +19,68 @@ from flask import current_app, Flask, redirect, url_for
 from flask_bcrypt import Bcrypt
 from flask_login import LoginManager 
 
+class config(object):
+    SECRET_KEY = 'secret'
+    DATA_BACKEND = 'cloudsql'
+    PROJECT_ID = 'whydeyyanp2'
+    CLOUDSQL_USER = 'root'
+    CLOUDSQL_PASSWORD = 'texasfight'
+    CLOUDSQL_DATABASE = 'mydb2'
+    CLOUDSQL_CONNECTION_NAME = 'whydeyyanp2:us-central1:library'
+
+
+    LOCAL_SQLALCHEMY_DATABASE_URI = (
+        'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
+            user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+            database=CLOUDSQL_DATABASE)
+
+    LIVE_SQLALCHEMY_DATABASE_URI = (
+        'mysql+pymysql://{user}:{password}@localhost/{database}'
+        '?unix_socket=/cloudsql/{connection_name}').format(
+            user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+            database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
+
+    if os.environ.get('GAE_INSTANCE'):
+        SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
+    else:
+        SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
+
 
 
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = 'secret'
-DATA_BACKEND = 'cloudsql'
-PROJECT_ID = 'whydeyyanp2'
-CLOUDSQL_USER = 'root'
-CLOUDSQL_PASSWORD = 'texasfight'
-CLOUDSQL_DATABASE = 'mydb2'
-CLOUDSQL_CONNECTION_NAME = 'whydeyyanp2:us-central1:library'
+app.config.from_object(config)
+# app.config['SECRET_KEY'] = 'secret'
+# DATA_BACKEND = 'cloudsql'
+# PROJECT_ID = 'whydeyyanp2'
+# CLOUDSQL_USER = 'root'
+# CLOUDSQL_PASSWORD = 'texasfight'
+# CLOUDSQL_DATABASE = 'mydb2'
+# CLOUDSQL_CONNECTION_NAME = 'whydeyyanp2:us-central1:library'
+# app.config['PROJECT_ID'] = 'whydeyyanp2'
 
 
-LOCAL_SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
-        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
-        database=CLOUDSQL_DATABASE)
+# LOCAL_SQLALCHEMY_DATABASE_URI = (
+#     'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
+#         user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+#         database=CLOUDSQL_DATABASE)
 
-LIVE_SQLALCHEMY_DATABASE_URI = (
-    'mysql+pymysql://{user}:{password}@localhost/{database}'
-    '?unix_socket=/cloudsql/{connection_name}').format(
-        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
-        database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
+# LIVE_SQLALCHEMY_DATABASE_URI = (
+#     'mysql+pymysql://{user}:{password}@localhost/{database}'
+#     '?unix_socket=/cloudsql/{connection_name}').format(
+#         user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+#         database=CLOUDSQL_DATABASE, connection_name=CLOUDSQL_CONNECTION_NAME)
 
-if os.environ.get('GAE_INSTANCE'):
-    SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
-else:
-    SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
+# if os.environ.get('GAE_INSTANCE'):
+#     SQLALCHEMY_DATABASE_URI = LIVE_SQLALCHEMY_DATABASE_URI
+# else:
+#     SQLALCHEMY_DATABASE_URI = LOCAL_SQLALCHEMY_DATABASE_URI
 
 
-app.config['SQLALCHEMY_DATABASE_URI'] = (
-    'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
-        user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
-        database=CLOUDSQL_DATABASE)
+# app.config['SQLALCHEMY_DATABASE_URI'] = (
+#     'mysql+pymysql://{user}:{password}@127.0.0.1:3306/{database}').format(
+#         user=CLOUDSQL_USER, password=CLOUDSQL_PASSWORD,
+#         database=CLOUDSQL_DATABASE)
 
 bcrypt = Bcrypt(app)
 login_manager = LoginManager(app)
